7ef4c6c07b63f85b156a143eeabda835d81afc29
Krystian Grzelak <krystian.grzelak@o2.pl>
2021-03-01 12:56:39 +0100

    Programmed resize buttons

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let numberOfDiv = 1;
+let divSize =100;
 
 document.querySelector('button.add').addEventListener('click', () => {
     const div = document.createElement('div')
+    div.style.width = `${divSize}px`;
+    div.style.height = `${divSize}px`;
+    div.style.lineHeight = `${divSize}px`;
     div.textContent = numberOfDiv;
     document.querySelector('.building').appendChild(div)
     numberOfDiv++
@@ -12,4 +16,24 @@ document.querySelector('button.rem').addEventListener('click', () => {
     document.querySelector('.building div:last-child').remove()
     numberOfDiv--
     }
+})
+
+document.getElementById('size-up').addEventListener('click', () => {
+    const divs = document.querySelectorAll('.building div');
+    divSize++;
+    divs.forEach(div => {
+        div.style.width = `${divSize}px`;
+        div.style.height = `${divSize}px`;
+        div.style.lineHeight = `${divSize}px`;
+    })
+})
+
+document.getElementById('size-down').addEventListener('click', () => {
+    const divs = document.querySelectorAll('.building div');
+    divSize--;
+    divs.forEach(div => {
+        div.style.width = `${divSize}px`;
+        div.style.height = `${divSize}px`;
+        div.style.lineHeight = `${divSize}px`;
+    })
 })
