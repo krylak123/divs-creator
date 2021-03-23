6ca8c191a18d922dbc994b29acc902d215c6528d
Krystian Grzelak <krystian.grzelak@o2.pl>
2021-03-23 14:12:08 +0100

    code rebuilding and appearance redesign

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
-let numberOfDiv = 1;
-let divSize = 100;
-let fontSize = 30;
-let divSizeSpan = document.querySelector('[data-option="size"] span');
-divSizeSpan.textContent = divSize + "px";
+const panel = document.querySelector('.panel')
+const spanSize = document.getElementById('spanSize')
 
-document.querySelector('button.add').addEventListener('click', () => {
+let number = 1;
+let divSize = 100;
+
+spanSize.textContent = `${divSize}px`;
+
+const addElement = () => {
+    if (panel.querySelectorAll('div').length >= 20) return alert(`20 div's max`)
     const div = document.createElement('div')
+    div.classList.add('panel__element')
+    div.setAttribute('data-index', `${number}`)
     div.style.width = `${divSize}px`;
     div.style.height = `${divSize}px`;
-    div.style.lineHeight = `${divSize}px`;
-    div.textContent = numberOfDiv;
-    document.querySelector('.building').appendChild(div)
-    numberOfDiv++
-})
+    div.textContent = number++;
+    panel.appendChild(div)
+}
 
-document.querySelector('button.rem').addEventListener('click', () => {
-    if (document.querySelector('.building div:last-child') !== null) {
-        document.querySelector('.building div:last-child').remove()
-        numberOfDiv--
+const remElement = () => {
+    if (panel.querySelector('div:last-child') !== null) {
+        panel.querySelector('div:last-child').remove()
+        number--;
     }
-})
+}
 
-document.getElementById('size-up').addEventListener('click', () => {
-    const divs = document.querySelectorAll('.building div');
+const sizeUpElement = () => {
+    if (divSize >= 200) return alert('max 200px')
+    const divs = panel.querySelectorAll('div')
     divSize += 5;
-    divs.forEach(div => {
-        div.style.width = `${divSize}px`;
-        div.style.height = `${divSize}px`;
-        div.style.lineHeight = `${divSize}px`;
+    divs.forEach(item => {
+        item.style.width = `${divSize}px`;
+        item.style.height = `${divSize}px`;
     })
-    divSizeSpan.textContent = divSize + "px";
-})
+    spanSize.textContent = `${divSize}px`;
+}
 
-document.getElementById('size-down').addEventListener('click', () => {
-    if (divSize > 0) {
-        const divs = document.querySelectorAll('.building div');
-        divSize -= 5;
-        divs.forEach(div => {
-            div.style.width = `${divSize}px`;
-            div.style.height = `${divSize}px`;
-            div.style.lineHeight = `${divSize}px`;
-        })
-        divSizeSpan.textContent = divSize + "px";
-    } else alert("size 0px! It can't be less.");
-})
+const sizeDownElement = () => {
+    if (divSize <= 50) return alert('min 50px')
+    const divs = panel.querySelectorAll('div')
+    divSize -= 5;
+    divs.forEach(item => {
+        item.style.width = `${divSize}px`;
+        item.style.height = `${divSize}px`;
+    })
+    spanSize.textContent = `${divSize}px`;
+}
+
+const shapeCircleElement = () => {
+    const divs = panel.querySelectorAll('div')
+    divs.forEach(item => item.style.borderRadius = `50%`)
+}
+
+const shapeSquareElement = () => {
+    const divs = panel.querySelectorAll('div')
+    divs.forEach(item => item.style.borderRadius = ``)
+}
+
+document.getElementById('addBtn').addEventListener('click', addElement)
+document.getElementById('remBtn').addEventListener('click', remElement)
+document.getElementById('upSizeBtn').addEventListener('click', sizeUpElement)
+document.getElementById('downSizeBtn').addEventListener('click', sizeDownElement)
+document.getElementById('shapeCircleBtn').addEventListener('click', shapeCircleElement)
+document.getElementById('shapeSquareBtn').addEventListener('click', shapeSquareElement)
